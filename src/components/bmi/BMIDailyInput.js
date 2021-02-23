b76f0855e45769460785bfbc354d34cb318eb3c7
andrewh-code <andrewh.code@gmail.com>
2021-02-23 16:12:45 -0500

    update bmi input layout

--- src/components/bmi/BMIDailyInput.js
+++ src/components/bmi/BMIDailyInput.js
@@ -34,75 +34,63 @@ export class BMIDailyInput extends Component {
         return (
             <div className="container border">
                 <form onSubmit={this.calculateBMI}>
-
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input"
-                            type="radio"
-                            name="gender"
-                            id="maleRadioInput"
-                            onChange={this.setGender}
-                            value="male" />
-                        <label className="form-check-label" htmlFor="maleRadioInput">Male</label>
-                    </div>
-
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input"
-                            type="radio"
-                            name="gender"
-                            id="femaleRadioInput"
-                            onChange={this.setGender}
-                            value="female" />
-                        <label className="form-check-label" htmlFor="femaleRadioInput">Female</label>
-                    </div>
                     <center>
-                        <div style={{ border: '1px solid white', width: '25%' }}>
-
-                            <input className="form-control border-0 shadow-none"
-                                type="text"
-                                name="height"
-                                placeholder="enter height in inches"
-                            />
+                        <div style={{marginTop: '1em'}}>
+                            <select id="weightUnits" style={{ fontSize: '0.75em', height: '100%', width: '30%'}}>
+                                <option value="">Measurement:</option>
+                                <option value="imperial">inches/lbs</option>
+                                <option value="metric">cm/Kgs</option>
+                            </select>
+                        </div>
+                    </center>
+                    <center>
+                        <div style={{marginTop: '1em'}}>
+                            <div className="form-check form-check-inline" style={{marginRight: '1em'}}>
+                                <input className="form-check-input"
+                                    type="radio"
+                                    name="gender"
+                                    id="maleRadioInput"
+                                    onChange={this.setGender}
+                                    value="male" />
+                                <label className="form-check-label" htmlFor="maleRadioInput" style={{fontSize: '0.75em'}}>Male</label>
+                            </div>
+                            <div className="form-check form-check-inline"style={{marginLeft: '1em'}}>
+                                <input className="form-check-input"
+                                    type="radio"
+                                    name="gender"
+                                    id="femaleRadioInput"
+                                    onChange={this.setGender}
+                                    value="female" />
+                                <label className="form-check-label" htmlFor="femaleRadioInput" style={{fontSize: '0.75em'}}>Female</label>
+                            </div>
+                        </div>
+                    </center>
+                    <center>
+                        <div style={{marginTop: '1em', width: '30%'}}>
+                                <input className="form-control border-0 shadow-none"
+                                    type="text"
+                                    name="height"
+                                    placeholder="enter height..."
+                                />
                         </div>
                     </center>
 
-                    <div>
-                        <div style={{ border: '1px solid white', display: 'inline', float: 'left', marginLeft: '5em', width: '6em' }}>
+                    <center>
+                    <div style={{marginTop: '1em', width: '50%'}}>
+                        
+                        <div style={{ display: 'inline', width: '50%' }}>
                             {gender != "female" ? <FontAwesomeIcon icon={faMale} size="10x" /> : <FontAwesomeIcon icon={faFemale} size="10x" />}
                         </div>
 
-                        <div style={{ display: 'inline', float: 'right', marginTop: '4em', marginRight: '5em' }}>
+                        <div style={{ display: 'inline', float: 'right', width: '40%', marginRight: '1em', marginTop: '4em'}}>
                             <input className="form-control border-0 shadow-none"
                                 type="text"
                                 name="height"
                                 placeholder="enter weight..."
                             />
-
-                            <div className="form-check form-check-inline">
-                                <input className="form-check-input"
-                                    type="radio"
-                                    name="weight"
-                                    id="lbRadioInput"
-                                    onChange={this.setGender}
-                                    value="lb" />
-                                <label className="form-check-label" htmlFor="lbRadioInput">pounds</label>
-                            </div>
-
-                            <div className="form-check form-check-inline">
-                                <input className="form-check-input"
-                                    type="radio"
-                                    name="weight"
-                                    id="kgRadioInput"
-                                    onChange={this.setGender}
-                                    value="kg" />
-                                <label className="form-check-label" htmlFor="kgRadioInput">kilograms</label>
-                            </div>
-
-
                         </div>
                     </div>
-
-
-
+                    </center>
                 </form>
 
             </div>
